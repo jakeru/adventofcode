30c8ee31278fadaa7cceced01cda581897c94c8a
Jakob Ruhe <jakob.ruhe@gmail.com>
2018-12-07 19:27:17 +0100

    Problem 6, subproblem 2, Easy implementation
    But it took a while to find out how to do it.

--- 2018/p6.py
+++ 2018/p6.py
@@ -70,20 +70,51 @@ def find_largest_area(coords):
     print("Largest area has index {} and an area of: {}".format(largest_area, areas[largest_area]))
     return areas[largest_area]
 
+def is_within_distance(coords, max_dist, p):
+    dist_sum = 0
+    for d in coords:
+        dist_sum += dist(d, p)
+        if dist_sum >= max_dist:
+            return False
+    return True
+
+def find_size_of_region_within(coords, max_dist):
+    print("Number of coords: {}".format(len(coords)))
+    margin = max_dist // len(coords)
+    print("Margin: {}".format(margin))
+    (min_c, max_c) = min_max(coords)
+    left = min_c.x - margin
+    right = max_c.x + margin
+    top = min_c.y - margin
+    bottom = max_c.y + margin
+    width = right - left
+    height = bottom - top
+    print("Coords min: {}, max: {}".format(min_c, max_c))
+    print("Locations: {} x {} = {}".format(width, height, width * height))
+    size_within = 0
+    for y in range(top, bottom + 1):
+        for x in range(left, right + 1):
+            if is_within_distance(coords, max_dist, Coord(x, y)):
+                assert(y != top and y != bottom and x != left and x != right)
+                size_within += 1
+    return size_within
+
 class TestThis(unittest.TestCase):
+    coords = [
+        Coord(1, 1),
+        Coord(1, 6),
+        Coord(8, 3),
+        Coord(3, 4),
+        Coord(5, 5),
+        Coord(8, 9),
+    ]
     def test_dist(self):
         self.assertEqual(dist(Coord(10, 20), Coord(11, 2)), 1+18)
         self.assertEqual(dist(Coord(10, 20), Coord(10, 20)), 0)
     def test_find_largest_area(self):
-        coords = [
-            Coord(1, 1),
-            Coord(1, 6),
-            Coord(8, 3),
-            Coord(3, 4),
-            Coord(5, 5),
-            Coord(8, 9),
-        ]
-        self.assertEqual(find_largest_area(coords), 17)
+        self.assertEqual(find_largest_area(self.coords), 17)
+    def test_find_size_of_region_within(self):
+        self.assertEqual(find_size_of_region_within(self.coords, 32), 16)
 
 if __name__ == "__main__":
     #unittest.main()
@@ -95,3 +126,5 @@ if __name__ == "__main__":
         coords.append(Coord(int(m.group(1)), int(m.group(2))))
     largest_area = find_largest_area(coords)
     print("The answer to subproblem 1 is: {}".format(largest_area))
+    size_within = find_size_of_region_within(coords, 10000)
+    print("The answer to subproblem 2 is: {}".format(size_within))
