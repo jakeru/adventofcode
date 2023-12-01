8d506ea4a7419fcc82bf7f1a5a6bca43327fe411
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-12-01 23:27:50 +0100

    Day 1 2023 after cleanup

--- 2023/day01.py
+++ 2023/day01.py
@@ -3,95 +3,58 @@
 # By Jakob Ruhe 2023-12-01
 
 import os
-import re
-import unittest
-from collections import defaultdict
-from collections import namedtuple
-import utils
 
 
 def parse_input(input):
     return input.strip().split("\n")
 
 
-def solve1(entries):
-    s = 0
-    for line in entries:
-        print(line)
-        digits = []
-        for c in line:
-            if c.isdigit():
-                digits.append(c)
-        print(digits)
-        s += int("".join([digits[0], digits[-1]]))
-    return s
+def digits():
+    return {str(value): value for value in range(1, 10)}
 
-# wrong: 17790
-# wrong: 17829
-# wrong: 17785
+
+def find_first_last(needle_map, haystack):
+    first = None
+    first_pos = len(haystack)
+    last = None
+    last_pos = -1
+    for k, v in needle_map.items():
+        p1 = haystack.find(k)
+        if p1 >= 0 and p1 < first_pos:
+            first = v
+            first_pos = p1
+        p2 = haystack.rfind(k)
+        if p2 >= 0 and p2 > last_pos:
+            last = v
+            last_pos = p2
+    return first, last
+
+
+def num_from_line(needle_map, line):
+    first, last = find_first_last(needle_map, line)
+    return int(str(first) + str(last))
+
+
+def solve(needle_map, entries):
+    return sum([num_from_line(needle_map, line) for line in entries])
+
+
+def solve1(entries):
+    return solve(digits(), entries)
+
+
+def digits_as_words():
+    digits = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    return {v: k + 1 for k, v in enumerate(digits)}
+
 
 def solve2(entries):
-    words = {"one": 1,
-             "two": 2,
-             "three": 3,
-             "four": 4,
-             "five": 5,
-             "six": 6,
-             "seven": 7,
-             "eight": 8,
-             "nine": 9}
-    s = 0
-    for line in entries:
-        print(line)
-        digits = []
-        digits_pos = []
-        for i, c in enumerate(line):
-            if c.isdigit():
-                digits.append(c)
-                digits_pos.append(i)
-        print(digits)
-        w1pos = None
-        w1 = None
-        for w in words.keys():
-            pos = line.find(w)
-            if pos >= 0 and (w1pos is None or pos < w1pos):
-                w1pos = pos
-                w1 = w
-        w2pos = None
-        w2 = None
-        for w in words.keys():
-            pos = line.rfind(w)
-            if pos >= 0 and (w2pos is None or pos > w2pos):
-                w2pos = pos
-                w2 = w
-        d1 = digits[0]
-        if w1pos is not None and w1pos < digits_pos[0]:
-            d1 = str(words[w1])
-        d2 = digits[-1]
-        if w2pos is not None and w2pos > digits_pos[-1]:
-            d2 = str(words[w2])
-        print(f"w {w1} {w1pos} {w2} {w2pos}")
-        print(d1, d2)
-        s += int("".join([d1, d2]))
-    return s
-
-
-# Execute tests with:
-# python3 -m unittest dayX
-class TestThis(unittest.TestCase):
-    input = """
-"""
-
-    def test1(self):
-        self.assertEqual(solve1(parse_input(self.input)), None)
-
-    def test2(self):
-        self.assertEqual(solve2(parse_input(self.input)), None)
+    return solve(digits() | digits_as_words(), entries)
 
 
 if __name__ == "__main__":
     problem_name = os.path.splitext(os.path.basename(__file__))[0]
     with open(f"input/{problem_name}.txt") as f:
         entries = parse_input(f.read())
-    # print(solve1(entries))
+    print(solve1(entries))
     print(solve2(entries))
