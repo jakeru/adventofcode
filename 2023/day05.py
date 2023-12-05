9f5f435a7a42e81274f88e666e7c64a2176ad60f
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-12-05 23:14:23 +0100

    Day 5 finally solved! Not refactored.

--- 2023/day05.py
+++ 2023/day05.py
@@ -14,23 +14,7 @@ def parse_input(input):
     return input.strip().split("\n")
 
 
-def find_location(seed, maps):
-    keys = list(maps.keys())
-    search = seed
-    for m in keys:
-        print(f"map {m}, searching for: {search}")
-        found = None
-        for e in maps[m]:
-            dest, source, num = e
-            print(f"{dest} {source} {num}")
-            if search >= source and search < source + num:
-                found = dest + search - source
-                break
-        search = found if found else search
-    return search
-
-
-def solve1(entries):
+def parse(entries):
     sections = "\n".join(entries).split("\n\n")
     print(sections)
     seeds = []
@@ -48,21 +32,128 @@ def solve1(entries):
             numbers = [int(w) for w in line.split()]
             print(f"{map_name}: {numbers}")
             maps[map_name].append(numbers)
+    return seeds, maps
 
-    print(f"maps: {list(maps.keys())}")
+
+def find_location(seed, maps):
+    keys = list(maps.keys())
+    search = seed
+    for m in keys:
+        print(f"map {m}, searching for: {search}")
+        found = None
+        for e in maps[m]:
+            dest, source, num = e
+            print(f"{dest} {source} {num}")
+            if search >= source and search < source + num:
+                found = dest + search - source
+                break
+        search = found if found else search
+    return search
+
+
+def solve1(entries):
+    seeds, maps = parse(entries)
 
     locations = {}
     for seed in seeds:
         locations[seed] = find_location(seed, maps)
-
 
     print(f"locations {locations}")
 
     return min(locations.values())
 
 
+Range = namedtuple("Range", ["min", "num"])
+
+# def find_lowest(search_min, search_num, maps):
+#     keys = list(reversed(maps.keys()))
+#     for m in keys:
+#         print(f"map {m}, searching for: {search_min} {search_num}")
+#         found = None
+#         for e in maps[m]:
+#             dest, source, num = e
+#             print(f"{dest} {source} {num}")
+#             if source >= search_min and source < search_min + search_num:
+#                 pass
+#
+#         search = found if found else search
+#     return search
+
+
+def range_str(r):
+    return f"[{r[0]}..{r[0] + r[1] - 1}]" if r[1] > 0 else "[]"
+
+
+def find_best_location_in_range(r, map_names, maps):
+    if not map_names:
+        print(f"Returning {r.min}")
+        return r.min
+    level_maps = sorted(maps[map_names[0]], key=lambda m: m[1])
+    best = None
+    prev = None
+    gaps = []
+    for lm in level_maps:
+        m = Range(lm[1], lm[2])
+        c_min = max(r.min, m.min)
+        c_max = min(r.min + r.num - 1, m.min + m.num - 1)
+        common = Range(c_min, c_max - c_min + 1)
+        if common.num > 0:
+            dest = Range(lm[0] + common.min - m.min, common.num)
+            print(
+                f"At {len(map_names)} {map_names[0]} Evaluating range {range_str(r)} in {range_str(m)}: overlap: {range_str(common)}, dest: {range_str(dest)}"
+            )
+            best_in_lm = find_best_location_in_range(dest, map_names[1:], maps)
+            if best is None or best_in_lm < best:
+                best = best_in_lm
+            if prev is None and r.min < m.min:
+                gaps.append(Range(r.min, m.min - r.min))
+                print(
+                    f"At {len(map_names)} {map_names[0]} Evaluating range {range_str(r)} in {range_str(m)}: adding gap: {range_str(gaps[-1])}"
+                )
+            elif prev is not None and prev.min + prev.num < common.min:
+                gaps.append(
+                    Range(prev.min + prev.num, common.min - (prev.min + prev.num))
+                )
+                print(
+                    f"At {len(map_names)} {map_names[0]} Evaluating range {range_str(r)} in {range_str(m)}: adding gap: {range_str(gaps[-1])}"
+                )
+            prev = common
+    if prev is None:
+        gaps.append(r)
+        print(
+            f"At {len(map_names)} {map_names[0]} Evaluating range {range_str(r)} adding full gap: {range_str(gaps[-1])}"
+        )
+    elif r.min + r.num > prev.min + prev.num:
+        gaps.append(Range(prev.min + prev.num, r.min + r.num - (prev.min + prev.num)))
+        print(
+            f"At {len(map_names)} {map_names[0]} Evaluating range {range_str(r)} adding last gap: {range_str(gaps[-1])}"
+        )
+
+    for g in gaps:
+        best_in_gap = find_best_location_in_range(g, map_names[1:], maps)
+        if best is None or best_in_gap < best:
+            best = best_in_gap
+
+    return best
+
+
+def find_best_location_in_ranges(ranges, map_names, maps):
+    best = None
+    for r in ranges:
+        best_in_range = find_best_location_in_range(r, map_names, maps)
+        if best is None or best_in_range < best:
+            best = best_in_range
+    return best
+
+
 def solve2(entries):
-    pass
+    seeds, maps = parse(entries)
+    map_names = list(maps.keys())
+
+    ranges = []
+    for i in range(0, len(seeds), 2):
+        ranges.append(Range(seeds[i], seeds[i + 1]))
+    return find_best_location_in_ranges(ranges, map_names, maps)
 
 
 # Execute tests with:
@@ -108,7 +199,7 @@ humidity-to-location map:
         self.assertEqual(solve1(parse_input(self.input)), 35)
 
     def test2(self):
-        self.assertEqual(solve2(parse_input(self.input)), None)
+        self.assertEqual(solve2(parse_input(self.input)), 46)
 
 
 if __name__ == "__main__":
