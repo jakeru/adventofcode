7cbbd20f3c03899851024ca162df8fbc6df0d84c
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-12-11 07:01:32 +0100

    Day 11 part 2

--- 2023/day11.py
+++ 2023/day11.py
@@ -16,7 +16,7 @@ def parse_input(input):
     return input.strip().split("\n")
 
 
-def expand_rows(grid):
+def expand_rows(grid, add):
     w = max([p.x for p in grid.keys()]) + 1
     h = max([p.y for p in grid.keys()]) + 1
     dy = 0
@@ -31,26 +31,26 @@ def expand_rows(grid):
                 grid_out[Point(p.x, dy)] = "#"
             dy += 1
         else:
-            dy += 2
+            dy += add
     return grid_out
 
 
-def expand_cols(grid):
+def expand_cols(grid, add):
     w = max([p.x for p in grid.keys()]) + 1
-    h = max([p.y for p in grid.keys()]) + 1
     dx = 0
     grid_out = {}
+    cols = {}
+    for p in grid:
+        if not p.x in cols:
+            cols[p.x] = []
+        cols[p.x].append(p)
     for x in range(w):
-        found = []
-        for y in range(h):
-            if grid.get(Point(x, y)):
-                found.append(Point(x, y))
-        if found:
-            for p in found:
+        if x in cols:
+            for p in cols[x]:
                 grid_out[Point(dx, p.y)] = "#"
             dx += 1
         else:
-            dx += 2
+            dx += add
     return grid_out
 
 
@@ -75,7 +75,7 @@ def solve1(entries):
             if c == "#":
                 grid[Point(x, y)] = "#"
     draw_grid(grid)
-    egrid = expand_cols(expand_rows(grid))
+    egrid = expand_cols(expand_rows(grid, 2), 2)
     print("Expanded:")
     draw_grid(egrid)
     stars = list(egrid.keys())
@@ -93,8 +93,28 @@ def solve1(entries):
     return sum(distances)
 
 
-def solve2(entries):
-    pass
+
+def solve2(entries, expand):
+    y = 0
+    grid = {}
+    for y, line in enumerate(entries):
+        for x, c in enumerate(line):
+            if c == "#":
+                grid[Point(x, y)] = "#"
+    egrid = expand_cols(expand_rows(grid, expand), expand)
+    stars = list(egrid.keys())
+    print(stars)
+    combinations = set(itertools.combinations(range(len(stars)), 2))
+    print(combinations)
+    print(len(combinations))
+    distances = []
+    for c in combinations:
+        p1 = stars[c[0]]
+        p2 = stars[c[1]]
+        dist = abs(p1.x - p2.x) + abs(p1.y - p2.y)
+        print(f"Distances between {c[0] + 1} and {c[1] + 1}: {dist}")
+        distances.append(dist)
+    return sum(distances)
 
 
 # Execute tests with:
@@ -117,7 +137,8 @@ class TestThis(unittest.TestCase):
         self.assertEqual(solve1(parse_input(self.input)), 374)
 
     def test2(self):
-        self.assertEqual(solve2(parse_input(self.input)), None)
+        self.assertEqual(solve2(parse_input(self.input), 10), 1030)
+        self.assertEqual(solve2(parse_input(self.input), 100), 8410)
 
 
 if __name__ == "__main__":
@@ -125,4 +146,4 @@ if __name__ == "__main__":
     with open(f"input/{problem_name}.txt") as f:
         entries = parse_input(f.read())
     print(solve1(entries))
-    print(solve2(entries))
+    print(solve2(entries, 1000000))
