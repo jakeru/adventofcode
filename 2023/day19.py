0c283e93e979300bef2b38b11ca3ffd9e3656d6e
Jakob Ruhe <jakob.ruhe@gmail.com>
2023-12-19 22:18:34 +0100

    Day 19 part 1 after some cleanup

--- 2023/day19.py
+++ 2023/day19.py
@@ -5,60 +5,52 @@
 import os
 import re
 import unittest
-from collections import defaultdict
-from collections import namedtuple
-import utils
 
 
 def parse_input(input):
     return input.strip().split("\n")
 
+
 def is_accepted(workflows, part, current):
-    while current != 'A' and current != 'R':
+    while current != "A" and current != "R":
         w = workflows[current]
         next = w[-1][-1]
         for rule in w[:-1]:
             lhs, op, rhs, target = rule
-            if op == '<':
-                if part[lhs] < rhs:
-                    next = target
-                    break
-            elif op == '>':
-                if part[lhs] > rhs:
-                    next = target
-                    break
-            else:
-                assert False
-        print(f"Moving part from {current} to {next}")
+            assert op in "<>"
+            if part[lhs] < rhs if op == "<" else part[lhs] > rhs:
+                next = target
+                break
         current = next
     print(f"Part {part} ended up in {current}")
-    return current == 'A'
+    return current == "A"
 
 
 def solve1(entries):
+    empty_line = entries.index("")
+
+    # Parse workflows
     workflows = {}
-    for line in entries:
-        if not line:
-            break
-        print(line)
-        # ([xmas])([<>])(\d+):([a-zA-Z]+)
-        name, rest = line.replace('{', ' ').replace('}', '').split()
+    for line in entries[:empty_line]:
+        m = re.match(r"(\S+){(\S+)}", line)
+        assert m
+        name, workflow = m[1], m[2].split(",")
         rules = []
-        workflow = rest.split(',')
         for w in workflow[:-1]:
-            rule, target = w.split(':')
-            op = '<' if '<' in rule else '>'
-            lhs, rhs = rule.split(op)
+            m = re.match(r"([xmas])([<>])(\d+):([a-zA-Z]+)", w)
+            assert m
+            lhs, op, rhs, target = m[1], m[2], m[3], m[4]
             rules.append((lhs, op, int(rhs), target))
         rules.append((None, None, None, workflow[-1]))
         workflows[name] = rules
+
     for name, w in workflows.items():
         print(f"W: {name} {w}")
 
+    # Parse parts
     parts = []
-    empty_line = entries.index("")
-    for line in entries[empty_line + 1:]:
-        ratings = re.findall(r'([xmas])=(\d+)', line)
+    for line in entries[empty_line + 1 :]:
+        ratings = re.findall(r"([xmas])=(\d+)", line)
         part = {}
         for r in ratings:
             part[r[0]] = int(r[1])
@@ -67,14 +59,9 @@ def solve1(entries):
     for p in parts:
         print(f"P: {p}")
 
-    accepted_parts = []
-    for p in parts:
-        if is_accepted(workflows, p, 'in'):
-            accepted_parts.append(p)
-
-    total = 0
-    for p in accepted_parts:
-        total += sum(p.values())
+    # Find accepted parts and take the sum of the ratings.
+    accepted_parts = [p for p in parts if is_accepted(workflows, p, "in")]
+    total = sum([sum(p.values()) for p in accepted_parts])
 
     return total
 
