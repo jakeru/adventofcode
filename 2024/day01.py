2f50e2ba17000f3ede5fcdfc11ff34763497924e
Jakob Ruhe <jakob.ruhe@gmail.com>
2024-12-01 10:15:45 +0100

    Day 1

---
+++ 2024/day01.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+# By Jakob Ruhe 2024-12-dd
+
+import os
+import re
+import unittest
+import sys
+from collections import defaultdict
+from collections import namedtuple
+import utils
+import aocd
+
+
+def parse_input(input):
+    return input.strip().split("\n")
+
+
+def solve_a(entries):
+    a = []
+    b = []
+    for line in entries:
+        #d = utils.parse_digits(line)
+        d = utils.parse_ints(line)
+        a.append(d[0])
+        b.append(d[1])
+        # a.append(int(d[0]))
+        # b.append(int(d[1]))
+    print(a)
+    print(b)
+    a = sorted(a)
+    b = sorted(b)
+    diffs = []
+    for i in range(len(a)):
+        diffs.append(abs(a[i] - b[i]))
+    return sum(diffs)
+
+
+def solve_b(entries):
+    a = []
+    b = []
+    for line in entries:
+        d = utils.parse_ints(line)
+        a.append(d[0])
+        b.append(d[1])
+    print(a)
+    print(b)
+    a = sorted(a)
+    b = sorted(b)
+    diffs = []
+    for i in range(len(a)):
+        num = 0
+        for ii in range(len(a)):
+            if b[ii] == a[i]:
+                num += 1
+        diffs.append(a[i] * num)
+    return sum(diffs)
+
+
+# Execute tests with:
+# python3 -m unittest dayX
+class TestThis(unittest.TestCase):
+    input = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"""
+
+    def test_a(self):
+        self.assertEqual(solve_a(parse_input(self.input)), None)
+
+    def test_b(self):
+        self.assertEqual(solve_b(parse_input(self.input)), None)
+
+
+if __name__ == "__main__":
+    problem_name = os.path.splitext(os.path.basename(__file__))[0]
+    with open(f"input/{problem_name}.txt") as f:
+        entries = parse_input(f.read())
+
+    parts = {
+        "a": solve_a,
+        "b": solve_b,
+    }
+
+    for part, solver in parts.items():
+        submit = part in sys.argv
+        answer = solver(entries)
+        print(f"Answer of part {part}:")
+        print(answer)
+        if answer is not None and submit:
+            aocd.submit(answer, part=part, reopen=False)
